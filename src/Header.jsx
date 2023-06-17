71539ca555a4f0466cb5fe4c7d2b7264edee267a
prabath1998 <prabhathudayanga1998@gmail.com>
2023-06-17 22:56:14 +0530

    init

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='p-5 bg-dark text-white'>Header</h1>
+    </header>
+  )
+}
+
+export default Header
